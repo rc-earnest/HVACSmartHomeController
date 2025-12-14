96bce429a8ce551bfb8d3e7301a09f7e9bffa9c2
rc-earnest <rc.earnest04@gmail.com>
2025-12-14 13:25:20 -0700

    Add project files.

---
+++ HVACSmartHomeController/HVACForm.vb
@@ -0,0 +1,12 @@
+﻿'Rudy Earnest
+'RCET 3371
+'Fall 2025
+'Smart Home HVAC Controller Application
+'Git link:
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+Public Class HVACForm
+
+End Class
